7a733a51f3b25b91f7b7b6755c5f46b659e51ae8
Predrag Rakic <prakic@renderedtext.com>
2016-12-19 11:47:30 +0100

    prolongue testing

--- spec/ii_spec.rb
+++ spec/ii_spec.rb
@@ -1,23 +1,8 @@
 require 'spec_helper'
 
 describe TestTestboosters do
-  it 'takes 1 sec' do
-    sleep 1
-    expect(true).to eq(true)
-  end
-
-  it 'takes 3 sec' do
-    sleep 3
-    expect(true).to eq(true)
-  end
-
-  it 'takes 5 sec' do
-    sleep 5
-    expect(true).to eq(true)
-  end
-
-  it 'takes 3 sec - II' do
-    sleep 3
+  it 'takes 35 sec - II' do
+    sleep 35
     expect(true).to eq(true)
   end
 end
--- spec/iii_spec.rb
+++ spec/iii_spec.rb
@@ -1,18 +1,9 @@
 require 'spec_helper'
 
 describe TestTestboosters do
-  it 'takes 3 sec' do
-    sleep 3
-    expect(true).to eq(true)
-  end
 
-  it 'takes 5 sec' do
-    sleep 5
-    expect(true).to eq(true)
-  end
-
-  it 'takes 3 sec - II' do
-    sleep 3
+  it 'takes 43 sec - II' do
+    sleep 43
     expect(true).to eq(true)
   end
 end
